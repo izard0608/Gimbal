4403c98de78e2a90a1f0c8d807986abf89ceb996
izard0608 <izard0608@126.com>
2025-11-05 14:33:39 +0800

    refactor: M3508 class refactor

--- UserCode/Inc/M3508.h
+++ UserCode/Inc/M3508.h
@@ -7,12 +7,12 @@
 
 #include "Motor.h"
 
-class M3508 : public Motor {
+class M3508 final : public Motor {
 public:
     M3508(float ratio, uint16_t esc_id, CAN_HandleTypeDef * hcan, const PID & ppid, const PID & spid);
-    void parse_can_msg_callback(const uint8_t rx_data[8]) final;
-    void write_tx() final;
-    float feedforward_intensity_calc(float current_angle) final;
+    void parse_can_msg_callback(const uint8_t rx_data[8]) override;
+    void write_tx() override;
+    float feedforward_intensity_calc(float current_angle) override;
 };
 
 #endif //GIMBAL_M3508_H
