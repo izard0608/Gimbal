6f12f745efff0f3196be31f4cecb3e9832b91d05
izard0608 <izard0608@126.com>
2025-11-09 21:34:12 +0800

    user_task_init func put into MX_FREERTOS_Init in freertos.c

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -25,7 +25,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "UserTask.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -71,7 +71,7 @@ void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
   */
 void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN Init */
-
+    user_task_init();
   /* USER CODE END Init */
 
   /* USER CODE BEGIN RTOS_MUTEX */
--- UserCode/Inc/UserTask.h
+++ UserCode/Inc/UserTask.h
@@ -2,9 +2,16 @@
 // Created by Izard on 2025/11/3.
 //
 
-#ifndef GIMBAL_USERTASK_H
-#define GIMBAL_USERTASK_H
+#ifndef GIMBAL_USER_TASK_H
+#define GIMBAL_USER_TASK_H
 
-void user_task_init();
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#endif //GIMBAL_USERTASK_H
+    void user_task_init();
+
+#ifdef __cplusplus
+}
+#endif
+#endif //GIMBAL_USER_TASK_H
